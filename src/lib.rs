c96fd110fc5b181edb51426d2937bbd74978cb0d
Yasin A. Yokus <ayokus@icloud.com>
2023-06-24 17:48:23 +0300

    feat: impl. identifer parser

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,27 @@ pub fn match_literal(expected: &'static str)
     }
 }
 
+fn identifier(input: &str) -> Result<(&str, String), &str> {
+    let mut matched = String::new();
+    let mut chars = input.chars();
+
+    match chars.next() {
+        Some(next) if next.is_alphabetic() => matched.push(next)
+        _ => return Err(input),
+    }
+
+    while let Some(next) = chars.next() {
+        if next.is_alphanumeric() || next == '-' {
+            matched.push(next);
+        } else {
+            break;
+        }
+    }
+
+    let next_index = matched.len();
+    Ok((&input[next_index..], matched))
+}
+
 
 
 #[cfg(test)]
